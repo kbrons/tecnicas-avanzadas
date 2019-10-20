6bbe91b40c5e0949c153ea5707082eb9b208fcfa
Kevin Bronstein <kevin.bronstein@southworks.com>
2019-10-20 18:50:48 -0300

    Add array of cuits happy path

--- financial-status/tests/repository.test.js
+++ financial-status/tests/repository.test.js
@@ -7,18 +7,18 @@ jest.mock('mariadb');
 describe('Financial Status Repository', () => {
     test('When calling get without an array or string, it should throw an error', async () => {
         const sut = new FinancialStatusRepository('', '', '');
-        
+
         await expect(sut.get(5)).rejects.toThrow('The parameter must be a string ir array of strings');
     });
 
     test('When calling get without parameters, it should throw an error', async () => {
         const sut = new FinancialStatusRepository('', '', '');
-        
+
         await expect(sut.get()).rejects.toThrow('A parameter is required');
     });
 
     test('When calling get with a CUIT, it should return its status', async () => {
-        const mockStatus = new FinancialStatus({status: 1, cuit: '29-23245438-8'});
+        const mockStatus = new FinancialStatus({ status: 1, cuit: '29-23245438-8' });
 
         mariadb.createPool.mockImplementation(() => {
             return {
@@ -31,7 +31,7 @@ describe('Financial Status Repository', () => {
         const sut = new FinancialStatusRepository('', '', '');
 
         const result = await sut.get(mockStatus.cuit);
-        
+
         expect(result).toStrictEqual(mockStatus);
     });
 
@@ -46,22 +46,30 @@ describe('Financial Status Repository', () => {
         const sut = new FinancialStatusRepository('', '', '');
 
         const result = sut.get(cuit);
-        
+
         expect(result).rejects.toThrowError(`The CUIT ${cuit} does not exist in the database`);
     });
 
-        test('When calling get with a CUIT that doesn\'t exist, it should throw an error', async () => {
-        const cuit = '29-23245438-8';
+    test('When calling get with an array of CUITs, it should return their statuses', async () => {
+        const mockStatuses = [
+            { status: 1, cuit: '29-23245438-8' },
+            { status: 3, cuit: '27-27245345-8' },
+            { status: 5, cuit: '23-23245438-8' },
+            { status: 2, cuit: '33-23245438-8' }
+        ];
+
+        const expectedResult = mockStatuses.map(({status, cuit}) => new FinancialStatus({ status, cuit }))
+
         mariadb.createPool.mockImplementation(() => {
             return {
-                query: jest.fn().mockImplementation(() => Promise.resolve([]))
+                query: jest.fn().mockImplementation(() => Promise.resolve(mockStatuses))
             };
         });
 
         const sut = new FinancialStatusRepository('', '', '');
 
-        const result = sut.get(cuit);
-        
-        expect(result).rejects.toThrowError(`The CUIT ${cuit} does not exist in the database`);
+        const result = await sut.get(mockStatuses.map(({cuit}) => cuit));
+
+        expect(result).toStrictEqual(expectedResult);
     });
 });
