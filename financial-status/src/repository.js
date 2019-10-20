5a3554a30889c1763d4eeb377ec2b62db2b2973e
Kevin Bronstein <kevin.bronstein@southworks.com>
2019-10-20 17:50:57 -0300

    fix implementation

--- financial-status/src/repository.js
+++ financial-status/src/repository.js
@@ -2,8 +2,8 @@ const mariadb = require('mariadb');
 const FinancialStatus = require('./model/financialStatus');
 
 class FinancialStatusRepository {
-    constructor(host, user, password, connectionLimit = 5) {
-        this._connectionPool = mariadb.createPool({host, user, password, connectionLimit});
+    constructor({host, user, password, database, connectionLimit = 5}) {
+        this._connectionPool = mariadb.createPool({host, user, password, database, connectionLimit});
     }
 
     get (argument) {
@@ -18,15 +18,14 @@ class FinancialStatusRepository {
     }
 
     async _getSeveral(cuits) {
-        const dbResult = await this._connectionPool.query('SELECT status FROM FINANCIAL_STATUS WHERE cuit in (?)', [cuits]);
-        
-        return dbResult.slice(0, 1).map(({status, cuit}) => new FinancialStatus({status, cuit}));
+        const dbResult = await this._connectionPool.query(`SELECT financial_status AS status, cuit FROM FINANCIAL_STATUS WHERE cuit IN (${cuits.map(() => '?').join(', ')})`, [...cuits]);
+        return dbResult.map(({status, cuit}) => new FinancialStatus({status, cuit}));
     }
 
     async _getSingle(cuit) {
-        const dbResult = await this._connectionPool.query('SELECT status FROM FINANCIAL_STATUS WHERE cuit = ?', [cuit]);
+        const dbResult = await this._connectionPool.query('SELECT financial_status AS status FROM FINANCIAL_STATUS WHERE cuit = ?', [cuit]);
         
-        if (dbResult.length < 2) {
+        if (dbResult.length < 1) {
             throw new Error(`The CUIT ${cuit} does not exist in the database`);
         }
 
