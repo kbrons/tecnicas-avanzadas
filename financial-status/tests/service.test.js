a72569821240d2bd6492f423c6fa79402c828661
Kevin Bronstein <kevin.bronstein@southworks.com>
2019-10-21 14:30:05 -0300

    Add cuit validation test cases

--- financial-status/tests/service.test.js
+++ financial-status/tests/service.test.js
@@ -7,4 +7,32 @@ describe('Financial Status Service', () => {
 
         expect(sut._validate(cuit)).toBeUndefined();
     });
+
+    it('Should throw an error when the CUIT is undefined', () => {
+        const cuit = undefined;
+        const sut = new Service();
+
+        expect(() => sut._validate(cuit)).toThrowError('The CUIT doesn\'t have a value');
+    });
+
+    it('Should throw an error when the CUIT is not separated by hyphens', () => {
+        const cuit = "23399163099";
+        const sut = new Service();
+
+        expect(() => sut._validate(cuit)).toThrowError('The CUIT should be separated by hyphens');
+    });
+
+    it('Should throw an error when the CUIT type is not valid', () => {
+        const cuit = "1-39916309-9";
+        const sut = new Service();
+
+        expect(() => sut._validate(cuit)).toThrowError('The CUIT type is not valid');
+    });
+
+    it('Should throw an error when the CUIT verification digit is not valid', () => {
+        const cuit = "23-39916309-5";
+        const sut = new Service();
+
+        expect(() => sut._validate(cuit)).toThrowError('The CUIT verification digit is not valid');
+    });
 });
