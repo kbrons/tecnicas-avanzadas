c69e3c98f9886eb71b747510b12cc9ee5ee8bf41
Kevin Bronstein <kevin.bronstein@southworks.com>
2019-10-23 08:51:31 -0300

    Add addOrUpdate implementation with tests

--- financial-status/src/repository.js
+++ financial-status/src/repository.js
@@ -1,6 +1,7 @@
 const mariadb = require('mariadb');
 const FinancialStatus = require('./model/financialStatus');
 const utils = require('./utils');
+const tableName = `FINANCIAL_STATUS`;
 
 class FinancialStatusRepository {
     constructor({host, user, password, database, connectionLimit = 5}) {
@@ -12,12 +13,12 @@ class FinancialStatusRepository {
     }
 
     async _getSeveral(cuits) {
-        const dbResult = await this._connectionPool.query(`SELECT financial_status AS status, cuit FROM FINANCIAL_STATUS WHERE cuit IN (${cuits.map(() => '?').join(', ')})`, [...cuits]);
+        const dbResult = await this._connectionPool.query(`SELECT financial_status AS status, cuit FROM ${tableName} WHERE cuit IN (${cuits.map(() => '?').join(', ')})`, [...cuits]);
         return dbResult.map(({status, cuit}) => new FinancialStatus({status, cuit}));
     }
 
     async _getSingle(cuit) {
-        const dbResult = await this._connectionPool.query('SELECT financial_status AS status FROM FINANCIAL_STATUS WHERE cuit = ?', [cuit]);
+        const dbResult = await this._connectionPool.query(`SELECT financial_status AS status FROM ${tableName} WHERE cuit = ?`, [cuit]);
         
         if (dbResult.length < 1) {
             throw new Error(`The CUIT ${cuit} does not exist in the database`);
@@ -29,8 +30,21 @@ class FinancialStatusRepository {
     }
 
     async addOrUpdate(statuses) {
-        
+        if(!statuses || !Array.isArray(statuses)) {
+            throw new Error(`The parameter is not valid`);
+        }
+
+        const mapToSQLInsertOrUpdate = createMapToSQLInsertOrUpdate(this._connectionPool);
+        const query = statuses.map(mapToSQLInsertOrUpdate).join('');
+        await this._connectionPool.query(query);
     }
 }
 
 module.exports = FinancialStatusRepository;
+
+const createMapToSQLInsertOrUpdate = pool => status => `
+    INSERT INTO ${tableName} (\`financial_status\`, \`dni\`, \`cuit\`, \`full_name\`) 
+    VALUES (${pool.escape(status.status)}, ${pool.escape(status.dni)}, ${pool.escape(status.cuit)}, ${pool.escape(status.name)})
+    ON DUPLICATE KEY UPDATE
+    financial_status=${pool.escape(status.status)}, full_name=${pool.escape(status.name)};
+`;
--- financial-status/tests/repository.test.js
+++ financial-status/tests/repository.test.js
@@ -5,16 +5,16 @@ const FinancialStatus = require('../src/model/financialStatus');
 jest.mock('mariadb');
 
 describe('Financial Status Repository', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     test('When calling get with a CUIT, it should return its status', async () => {
         const mockStatus = new FinancialStatus({ status: 1, cuit: '29-23245438-8' });
 
-        mariadb.createPool.mockImplementation(() => {
-            return {
-                query: jest.fn().mockImplementation(() => Promise.resolve([{
-                    status: mockStatus.status
-                }]))
-            };
-        });
+        mockMariaDb(() => Promise.resolve([{
+            status: mockStatus.status
+        }]));
 
         const sut = new FinancialStatusRepository('', '', '');
 
@@ -25,11 +25,7 @@ describe('Financial Status Repository', () => {
 
     test('When calling get with a CUIT that doesn\'t exist, it should throw an error', async () => {
         const cuit = '29-23245438-8';
-        mariadb.createPool.mockImplementation(() => {
-            return {
-                query: jest.fn().mockImplementation(() => Promise.resolve([]))
-            };
-        });
+        mockMariaDb(() => Promise.resolve([]));
 
         const sut = new FinancialStatusRepository('', '', '');
 
@@ -46,18 +42,59 @@ describe('Financial Status Repository', () => {
             { status: 2, cuit: '33-23245438-8' }
         ];
 
-        const expectedResult = mockStatuses.map(({status, cuit}) => new FinancialStatus({ status, cuit }))
+        const expectedResult = mockStatuses.map(({ status, cuit }) => new FinancialStatus({ status, cuit }));
 
-        mariadb.createPool.mockImplementation(() => {
-            return {
-                query: jest.fn().mockImplementation(() => Promise.resolve(mockStatuses))
-            };
-        });
+        mockMariaDb(() => Promise.resolve(mockStatuses));
 
         const sut = new FinancialStatusRepository('', '', '');
 
-        const result = await sut.get(mockStatuses.map(({cuit}) => cuit));
+        const result = await sut.get(mockStatuses.map(({ cuit }) => cuit));
 
         expect(result).toStrictEqual(expectedResult);
     });
+
+    test('When calling addOrUpdate without a parameter, it should throw an error', async () => {
+        const sut = new FinancialStatusRepository('', '', '');
+
+        const result = sut.addOrUpdate();
+
+        expect(result).rejects.toThrowError(`The parameter is not valid`);
+    });
+
+    test('When calling addOrUpdate with a non-array parameter, it should throw an error', async () => {
+        const sut = new FinancialStatusRepository('', '', '');
+
+        const result = sut.addOrUpdate('test');
+
+        expect(result).rejects.toThrowError(`The parameter is not valid`);
+    });
+
+    test('When calling addOrUpdate with an array of financial statuses, it should ', async () => {
+        const mockFinancialStatuses = [
+            new FinancialStatus({ status: 5, dni: "27170936", cuit: "30-27170936-9", name: "Jock Shipperbottom" }),
+            new FinancialStatus({ status: 5, dni: "81704837", cuit: "33-81704837-7", name: "Cale Liveing" })
+        ];
+
+        const mockQueryInstance = mockMariaDb(() => Promise.resolve({ affectedRows: 2, insertId: 2, warningStatus: 0 }))
+
+        const sut = new FinancialStatusRepository('', '', '');
+
+        await sut.addOrUpdate(mockFinancialStatuses);
+
+        expect(mockQueryInstance).toHaveBeenCalled();
+        expect(mockQueryInstance.mock.calls[0][0]).toEqual(expect.stringContaining(mockFinancialStatuses[0].cuit));
+        expect(mockQueryInstance.mock.calls[0][0]).toEqual(expect.stringContaining(mockFinancialStatuses[1].cuit));
+    });
 });
+
+const mockMariaDb = queryImplementation => {
+    const mockQueryInstance = jest.fn(queryImplementation);
+    mariadb.createPool.mockImplementation(() => {
+        return {
+            query: mockQueryInstance,
+            escape: value => value
+        };
+    });
+
+    return mockQueryInstance;
+};
